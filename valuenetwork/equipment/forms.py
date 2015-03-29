70b6ef1c56b3923d246bbcc0ba38fe9122c1bb89
Lynn Foster <foster.j.lynn@gmail.com>
2015-03-29 09:49:11 -0500

    Missing pieces from new equipment app.

---
+++ valuenetwork/equipment/forms.py
@@ -0,0 +1,36 @@
+import sys
+import datetime
+from decimal import *
+from django import forms
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from valuenetwork.valueaccounting.models import *
+
+class EquipmentUseForm(forms.ModelForm):
+    event_date = forms.DateField(
+        required=True, 
+        label="Date of use",
+        widget=forms.TextInput(attrs={'class': 'input-small date-entry', }))
+    from_agent = forms.ModelChoiceField(
+        required=True,
+        queryset=EconomicAgent.objects.all(),
+        label="Who is using",
+        empty_label=None,
+        widget=forms.Select(
+            attrs={'class': 'chzn-select'})) 
+    quantity = forms.DecimalField(required=True,
+        label="Number of hours",
+        widget=forms.TextInput(attrs={'value': '1.00', 'class': 'quantity  input-small'}))
+        
+    class Meta:
+        model = EconomicEvent
+        fields = ('from_agent', 'event_date', 'quantity')
+
+    def __init__(self, resource=None, context_agent=None, *args, **kwargs):
+        super(EquipmentUseForm, self).__init__(*args, **kwargs)
+        #import pdb; pdb.set_trace()
+        if context_agent:
+            if resource:
+                self.fields["from_agent"].queryset = resource.equipment_users(context_agent=context_agent)
+            

8f0aaa1dfa97ce176c4734b6fad4a009c94ed97b
Lynn Foster <foster.j.lynn@gmail.com>
2015-07-14 09:19:38 -0500

    Add notification to management file.

--- valuenetwork/valueaccounting/management.py
+++ valuenetwork/valueaccounting/management.py
@@ -6,6 +6,7 @@ if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
+        notification.create_notice_type("valnet_join_task", _("Join Task"), _("a colleaque wants to help with this task"), default=2)
         notification.create_notice_type("valnet_help_wanted", _("Help Wanted"), _("a colleague requests help that fits your skills"), default=2)
         notification.create_notice_type("valnet_new_task", _("New Task"), _("a new task was posted that fits your skills"), default=2)
         notification.create_notice_type("valnet_new_todo", _("New Todo"), _("a new todo was posted that is assigned to you"), default=2)

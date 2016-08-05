1e3d58c330ea6992703deaf37e90cfaf8793575b
Bob Haugen <bob.haugen@gmail.com>
2016-08-05 15:54:20 -0500

    add agent to admin MembershipRequest display fields

--- work/admin.py
+++ work/admin.py
@@ -4,4 +4,7 @@ from valuenetwork.valueaccounting.actions import export_as_csv
 
 admin.site.add_action(export_as_csv, 'export_selected objects')
 
-admin.site.register(MembershipRequest)
+class MembershipRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'agent', )
+    
+admin.site.register(MembershipRequest, MembershipRequestAdmin)

fb37adc28d0ae789ca564c6efa5ba8fbd5dac8a6
Ale Fernandez <skoria@gmail.com>
2016-08-06 10:01:35 +0200

    Update models.py
    corrected according to https://titanpad.com/freedomcoopmemberform

--- work/models.py
+++ work/models.py
@@ -13,12 +13,12 @@ MEMBERSHIP_TYPE_CHOICES = (
 class MembershipRequest(models.Model):
     request_date = models.DateField(auto_now_add=True, blank=True, null=True, editable=False)
     name = models.CharField(_('Name'), max_length=255)
-    surname = models.CharField(_('Surname (for individuals)'), max_length=255, blank=True)
+    surname = models.CharField(_('Surname (for individual memberships)'), max_length=255, blank=True)
     requested_username = models.CharField(_('Requested username'), max_length=32)
     email_address = models.EmailField(_('Email address'), max_length=96,)
     #    help_text=_("this field is optional, but we can't contact you via email without it"))
     phone_number = models.CharField(_('Phone number'), max_length=32, blank=True, null=True)
-    address = models.CharField(_('Address (where do you live?)'), max_length=255, blank=True)
+    address = models.CharField(_('Where do you live?'), max_length=255, blank=True)
     native_language = models.CharField(_('Languages'), max_length=255, blank=True)
     type_of_membership = models.CharField(_('Type of membership'),
         max_length=12, choices=MEMBERSHIP_TYPE_CHOICES,
@@ -33,20 +33,20 @@ class MembershipRequest(models.Model):
     #    help_text=_("you don't offer any product or service but want to consume through it and support the cooperative"))
     number_of_shares = models.IntegerField(_('Number of shares'),
         default=1,
-        help_text=_("How many shares do you want to underwrite? Each share is worth 30 Euro (600 Faircoin)"))
+        help_text=_("How many shares would you like to underwrite? Each share is worth 30 Euros (600 Faircoin)"))
     #work_for_shares = models.BooleanField(_('work for one share'), default=False,
     #    help_text=_("You can get 1 share for 6 hours of work. If you choose this option, we will send you a list of tasks and the deadline. You won't have full access before the tasks are accomplished."))
     description = models.TextField(_('Description'),
-        help_text=_("Describe your project or collective and the skills or abilities you can offer to the cooperative"))
+        help_text=_("Describe your project or collective and the skills or abilities you can offer the cooperative"))
     website = models.CharField(_('Website'), max_length=255, blank=True)
     fairnetwork = models.CharField(_('FairNetwork username'), max_length=255, blank=True,
         help_text = _("The username you use in the FairNetwork at <a href='https://fair.coop' target='_blank'>fair.coop</a>"))
     usefaircoin = models.CharField(_('UseFaircoin profile'), max_length=255, blank=True,
-        help_text = _("If you are in the directory at <a href='https://use.fair-coin.org' target='_blank'>use.fair-coin.org</a> please put the URL to your profile."))
+        help_text = _("If you are in the directory at <a href='https://use.fair-coin.org' target='_blank'>use.fair-coin.org</a> please add the URL to your profile."))
     fairmarket = models.CharField(_('FairMarket shop'), max_length=255, blank=True,
-        help_text = _("If you have an online shop at <a href='https://market.fair.coop' target='_blank'>market.fair.coop</a> please put the URL to your fair shop."))
+        help_text = _("If you have an online shop at <a href='https://market.fair.coop' target='_blank'>market.fair.coop</a> please add the URL to your fair shop."))
     #how_do_you_know_fc = models.TextField(_('How do you know Freedom Coop?'), blank=True,)
-    known_member = models.CharField(_('Is there any FairCoop participant who can give references about you? If so, who?'), max_length=255, blank=True)
+    known_member = models.CharField(_('Are there any FairCoop participant(s) who can give references about you? If so, who?'), max_length=255, blank=True)
     comments_and_questions = models.TextField(_('Comments and questions'), blank=True,)
 
     agent = models.ForeignKey(EconomicAgent,

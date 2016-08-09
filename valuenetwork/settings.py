c3146590e4f77b6ddc8d222c37f13c9081ae7102
XaviP <pezbaloo@riseup.net>
2016-08-09 17:41:52 +0200

    middlewares: changing order and adding django.middleware.locale.LocaleMiddleware

--- valuenetwork/settings.py
+++ valuenetwork/settings.py
@@ -114,16 +114,17 @@ LOGIN_EXEMPT_URLS = (
 
 
 MIDDLEWARE_CLASSES = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "valuenetwork.login_required_middleware.LoginRequiredMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'valuenetwork.login_required_middleware.LoginRequiredMiddleware',
     #'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 

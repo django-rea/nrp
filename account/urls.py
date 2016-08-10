9b1cb0d9fdc3794aa33aa4b6d3b6324f829c602a
Bob Haugen <bob.haugen@gmail.com>
2016-08-10 09:51:51 -0500

    add account settings to work user menu

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from account.views import SignupView, LoginView, LogoutView, DeleteView
 from account.views import ConfirmEmailView
 from account.views import ChangePasswordView, PasswordResetView, PasswordResetTokenView
-from account.views import SettingsView
+from account.views import SettingsView, WorkSettingsView
 
 
 urlpatterns = patterns("",
@@ -15,5 +15,6 @@ urlpatterns = patterns("",
     url(r"^password/reset/$", PasswordResetView.as_view(), name="account_password_reset"),
     url(r"^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", PasswordResetTokenView.as_view(), name="account_password_reset_token"),
     url(r"^settings/$", SettingsView.as_view(), name="account_settings"),
+    url(r"^worksettings/$", WorkSettingsView.as_view(), name="work_account_settings"),
     url(r"^delete/$", DeleteView.as_view(), name="account_delete"),
 )

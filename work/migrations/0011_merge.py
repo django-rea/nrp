22bb24283ea4b3c1561b9ad9c49ceae5cb06011b
bumbum <bumbum@cooperativa.cat>
2016-09-05 22:34:01 +0200

    merged migrations

---
+++ work/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('work', '0010_auto_20160902_0436'),
+        ('work', '0008_merge'),
+    ]
+
+    operations = [
+    ]
